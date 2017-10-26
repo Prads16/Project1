ec4ba4ade325ba92d1499d9ef239809d38819021
Pradnya R Ambre <pra22@afsconnect1.njit.edu>
2017-10-26 18:11:47 -0400

    Homepage

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 
-ini_set('display_errors', 'On');//Turning the debug messages ON
-error_reporting(E_ALL);
-
 //Class to load classes when the program fails for calling the missing class
 class Manage 
 {
@@ -46,13 +43,11 @@ abstract class page
 
     public function __construct()
     {
-        /*$this->html .= '<html>';
-        $this->html .= '<link rel = "stylesheet" href = "styles.css">';
-        $this->html .= '<body>';*/
+        $this->html .= '<html>';
+        $this->html .= '<body>';
     }
     public function __destruct()
     {
-        //$this->html .= '</body></html>';
         stringfunctions::printThis($this->html);
     }
 
@@ -66,71 +61,5 @@ abstract class page
         print_r($_POST);
     }
 }
-
-/*class homepage extends page 
-{
-    public function get()
-    {
-        //Displaying the HTML form for uploading the CSV files
-        $form = '<form method="post" enctype="multipart/form-data">';
-        $form .= '<input type="file" name="fileToUpload" id="fileToUpload">';
-        $form .= '<input type="submit" value="Upload File" name="submit">';
-        $form .= '</form> ';
-        $this->html .= '<h1>Upload CSV File for Conversion</h1>';
-        $this->html .= $form;
-    }
-
-    public function post() 
-    {
-        $saved_in_dir = "Uploads/";//Assigning the directory in which the csv files will be saved
-        $saved_file = $saved_in_dir . basename($_FILES["fileToUpload"]["name"]);
-        $uploadOk = 1;
-        $csvFileType = pathinfo($saved_file,PATHINFO_EXTENSION);
-        $csvFileName = pathinfo($saved_file,PATHINFO_BASENAME);
-        move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $saved_file); //Saving the csv file in the directory on AFS
-        header('Location: index.php?page=htmlTable&filename='.$saved_file);//Sending the HTTP Header
-    }
-}*/
-
-/*class htmlTable extends page 
-{
-    public function get()
-    {
-        //$csvname = $_GET['fileName'];
-        $convert = fopen($_REQUEST['filename'], "r");//Reading the .csv File 
-        echo '<table border = "3">';//Generating a table
-        $tabledata = (fgetcsv($convert));//Reading the file for headers of the file
-        //Displaying the header row
-        echo '<tr>';
-        foreach ($tabledata as $headercolumn)
-        {
-            echo "<th>$headercolumn</th>";
-        }
-        echo '</tr>';
-    
-        while (!feof($convert))//Read all the rows till the end of file
-        {
-            $tabledata = (fgetcsv($convert));// Reading the content of the file
-            //echo "<table border = '3'>";
-            //for ($row = 0; $row < sizeof($tabledata); $row++)
-            echo '<tr>';
-            for ($col = 0; $col < sizeof($tabledata); $col++)
-            {
-                echo "<td>$tabledata[$col]</td>";   // Displaying the column data line by line       
-            }
-            echo '</tr>';  
-        }
-        echo '</table>';//Displaying the table
-        fclose($convert);//Closing the file
-    }
-}*/
-
-/*class stringFunctions 
-{
-     static public function printThis($inputText) 
-     {
-        return print($inputText);
-     }
-}*/
 
 ?>
